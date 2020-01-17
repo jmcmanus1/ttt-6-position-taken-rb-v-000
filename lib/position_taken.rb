561cfdc3301581a3348a9bcbc264da6376028cbf
Julie  McManus <julieannesays@gmail.com>
2020-01-17 16:52:59 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,8 @@
 def position_taken?(board,index)
-  if board[index] == " " ||  board[index] == ""
-  elsif board[index] == nil
+  taken = nil 
+  if (board[index] == " " ||  board[index] == "" || board[index] == nil
+    taken = false 
+  else
+    taken = true 
   end
 end
